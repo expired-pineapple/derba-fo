6d53a0ffb7dfd463a34a1eebbc82057213be5539
Idelilah <ruthtirusew944@gmail.com>
2023-08-23 17:02:13 +0300

    feat: Report Generation for fo and fuel added

---
+++ src/pages/Reports/FOReport.vue
@@ -0,0 +1,171 @@
+<template>
+  <VCard title="Freight Order Report">
+    <VCardText>
+      <VForm class="d-flex mb-4">
+        <VTextField
+          v-model="initialDate"
+          label="Initial Date"
+          clearable
+          variant="outlined"
+          hide-details
+          type="date"
+          class="mr-2"
+        />
+        <VTextField
+          v-model="finalDate"
+          label="Final Date"
+          clearable
+          variant="outlined"
+          hide-details
+          type="date"
+        />
+        <VBtn
+          variant="outline"
+          color="primary"
+          class="ml-2"
+          @click="handleDownloadPDF"
+        >
+          <VIcon>mdi-download</VIcon>
+          Download Report
+        </VBtn>
+      </VForm>
+      <EasyDataTable
+        table-class-name="customize-table"
+        rows-per-page="10"
+        :headers="headers"
+        :items="items"
+        :loading="loading"
+        :search-field="searchField"
+        :search-value="searchValue"
+      >
+        <template #loading>
+          <p>Loading Data</p>
+        </template>
+        <template #empty-message>
+          <div class="text-center">
+            <p>No Data found.</p>
+          </div>
+        </template>
+      </EasyDataTable>
+    </VCardText>
+  </VCard>
+</template>
+
+<script lang="ts" setup>
+import ListLayout from "@/views/pages/page-layout/ListLayout.vue"
+import router from "@/router"
+import { ref, onMounted, onBeforeMount } from "vue"
+import { useStore } from 'vuex'
+
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
+
+const headers = [
+  { text: 'Shipment Code', value: 'ShipmentCode', sortable: true },
+  { text: 'FO No', value: 'foNo' },
+  { text: 'Customer', value: 'CustomerID.cmrName' },
+  { text: 'Material', value: 'MtrId.mtrName' },
+  { text: 'Trip', value: 'TrpId.trpRouteName' },
+  { text: 'FO Date', value: 'foDate', sortable: true },
+  { text: 'FO Time', value: 'foTime' },
+  { text: 'Open Km', value: 'foOpenKm' },
+  { text: 'Return Date', value: 'foRtnDate' },
+  { text: 'Return Time', value: 'foRtnTime' },
+  { text: 'Close Km', value: 'foCloseKm' },
+  { text: 'Meter Quantity', value: 'foMtrQuantity' },
+]
+
+
+const store = useStore()
+const items = ref([])
+const loading = ref(true)
+
+const initialDate = ref(null)
+const finalDate = ref(null)
+
+onBeforeMount(async () => {
+  try {
+    await store.dispatch("fetchFos")
+    items.value = store.getters.fos
+  } catch (err) {
+    console.error("Error dispatching fetch action:", err)
+  } finally {
+    loading.value = false
+  }
+})
+
+const handleDownloadPDF = () => {
+  const doc = new jsPDF()
+
+  // Add logo and text to the PDF
+  const logoImg = new Image()
+
+  logoImg.src = './logo.png'
+
+  logoImg.onload = function() {
+    doc.addImage(logoImg, 'PNG', 10, 20, 20, 20) 
+    doc.setFontSize(20)
+    doc.text('Derba Midroc Group', 30, 25)
+    doc.setFontSize(10)
+    doc.text('Ras Desta Damitew St.,', 30, 30)
+    doc.setFontSize(8)
+    doc.text('Nani building 9th-12th Floor', 30, 33)
+    doc.setFontSize(8)
+    doc.text('Addis Ababa, Ethiopia', 30, 36)
+    doc.setFontSize(15)
+    doc.setFont('helvetica', 'bold')
+    doc.text('Fuel Report', 80, 50) 
+    generatePDF(doc) 
+  }
+}
+
+const generatePDF = doc => {
+  // Filter data based on initialDate and finalDate
+  const filteredData = items.value.filter(item => {
+    const itemDate = new Date(item.foDate)
+    const initialDateValue = new Date(initialDate.value)
+    const finalDateValue = new Date(finalDate.value)
+
+    return itemDate >= initialDateValue && itemDate <= finalDateValue
+  })
+
+  if (filteredData.length === 0) {
+    doc.setFontSize(12)
+    doc.setFont('helvetica', 'normal')
+    doc.text('No data found on Specified Date', 70, 80)
+    doc.save('freight-order-report.pdf')
+
+    return
+  }
+
+  const jsonData = filteredData.map(obj => {
+    const rObj = []
+
+    rObj.push(obj.ShipmentCode)
+    rObj.push(obj.foNo)
+    rObj.push(obj.CustomerID.cmrName)
+    rObj.push(obj.MtrId.mtrName)
+    rObj.push(obj.foMtrQuantity)
+    rObj.push(obj.TrpId.trpRouteName)
+    rObj.push(obj.foDate)
+    rObj.push(obj.foTime)
+    rObj.push(obj.foRtnDate)
+    rObj.push(obj.foRtnTime)
+
+    return rObj
+  })
+
+  doc.setFontSize(12)
+  doc.setFont('helvetica', 'normal')
+
+  doc.autoTable({
+    headStyles: { fillColor: [15,  142, 61], minCellWidth: 12, lineWidth: 0.1 },
+    head: [['Shipment Code', 'FO No', 'Customer', 'Material', 'Qty', 'Route', 'Date', 'Time', 'Return Date', 'Return Time']],
+
+    body: jsonData,
+    startY: 55, 
+  })
+
+  doc.save('freight-order-report.pdf')
+}
+</script>
---
+++ src/pages/Reports/FuelReport.vue
@@ -0,0 +1,108 @@
+<template>
+  <ListLayout v-bind="props" />
+</template>
+
+<script lang="ts" setup>
+import ListLayout from "@/views/pages/page-layout/ListLayout.vue"
+import router from "@/router"
+import { ref, onMounted, onBeforeMount } from "vue"
+import { useStore } from 'vuex'
+import jsPDF from 'jspdf'
+import 'jspdf-autotable' // Import the plugin
+
+
+const headers = [
+  { text: 'Shipment Code', value: 'FoId.ShipmentCode' },
+  { text: 'Fuel Station', value: 'fuelStationID.fuelStation' },
+  { text: 'Fuel Cap No', value: 'fuelCapNo' },
+  { text: 'Fuel Amount', value: 'fuelAmt' },
+  { text: 'Fuel Cash Birr', value: 'fuelCashBirr' },
+  { text: 'Fuel Payment Type', value: 'fuelPmtType' },
+  { text: 'Fuel Cap Remark', value: 'fuelCapRmk' },
+]
+
+const store = useStore()
+const items = ref([])
+const loading = ref(true)
+
+onBeforeMount(async () => {
+  try {
+    await store.dispatch('fetchFuels')
+    items.value = store.getters.fuels
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
+})
+
+const handleDownloadPDF = () => {
+  const doc = new jsPDF()
+
+  // Add logo and text to the PDF
+  const logoImg = new Image()
+
+  logoImg.src = './logo.png'
+
+  logoImg.onload = function() {
+    doc.addImage(logoImg, 'PNG', 10, 20, 20, 20)
+    doc.setFontSize(20)
+    doc.text('Derba Midroc Group', 30, 25)
+    doc.setFontSize(10)
+    doc.text('Ras Desta Damitew St.,', 30, 30)
+    doc.setFontSize(8)
+    doc.text('Nani building 9th-12th Floor', 30, 33)
+    doc.setFontSize(8)
+    doc.text('Addis Ababa, Ethiopia', 30, 36)
+    doc.setFontSize(12)
+    doc.text('Fuel Report', 80, 50)
+    doc.setFont('helvetica', 'bold')
+    generatePDF(doc)
+  }
+}
+
+const generatePDF = doc => {
+  const jsonData = items.value
+
+  const data = jsonData.map(obj => {
+    const rObj = []
+
+    rObj.push(obj.FoId.ShipmentCode)
+    rObj.push(obj.fuelStationID.fuelStation)
+    rObj.push(obj.fuelCapNo)
+    rObj.push(obj.fuelAmt)
+    rObj.push(obj.fuelCashBirr)
+    rObj.push(obj.fuelPmtType)
+    rObj.push(obj.fuelCapRmk)
+
+    return rObj
+  })
+
+  doc.setFontSize(12)
+  doc.setFont('helvetica', 'normal')
+
+  doc.autoTable({
+    headStyles: { fillColor: [15, 142, 61] },
+    head: [['Shipment Code', 'Fuel Station', 'Fuel Cap No', 'Fuel Amount', 'Fuel Cash Birr', 'Fuel Payment Type', 'Fuel Cap Remark']],
+    body: data,
+    startY: 55, // Adjust the starting Y position as needed to make space for the logo and text
+  })
+
+  doc.save('fuel-report.pdf')
+}
+
+const props = {
+  header: "Fuel Report",
+  button: {
+    text: "Add Tire Status",
+    to: "/tire-status/register",
+  },
+  buttonVisible: false,
+  tableHeader: {
+    headers,
+    items,
+  },
+  handleDownloadPDF,
+  placeholder: "Search via payment type",
+}
+</script>
